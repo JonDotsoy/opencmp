34a7c0c54430a7ba6b3ef4bdbdc5a9f06c8e48b2
Jonathan Delgado <hi@jon.soy>
2025-10-01 10:57:07 -0300

    fix: update index page to use dynamic base URL for redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jondotsoy.github.io",
+  base: "/opencmp",
   vite: {
     plugins: [tailwindcss()],
   },
